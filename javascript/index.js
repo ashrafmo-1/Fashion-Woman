b06994e67b61a804b359085c5379d84998da7265
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-03 14:21:52 +0300

    edit responsive input search, edit swiper first-show, add th swiper for designer

---
+++ javascript/index.js
@@ -0,0 +1,55 @@
+var swiper = new Swiper(".card-slider", {
+    spaceBetween: 10,
+    loop: true,
+    speed: 1000,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+    },
+});
+
+var swiper = new Swiper(".card-slider-designers", {
+    spaceBetween: 5,
+    loop: true,
+    speed: 1000,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 8,
+        },
+    },
+});
